cb1628dae3d1066e3009db65f6cd375f2b648c04
ARJUNCSE43 <125268867+ARJUNCSE43@users.noreply.github.com>
2025-04-18 23:45:46 +0600

    Add files via upload

---
+++ At coder DP/C - Vacation.cpp
@@ -0,0 +1,42 @@
+
+#include<bits/stdc++.h>
+#define vi vector<int>
+#define ll long long
+
+using namespace std;
+void solve()
+{
+    int N;
+    cin >> N;
+
+    vector<vector<int>> happiness(N, vector<int>(3));
+    for (int i = 0; i < N; ++i) {
+        cin >> happiness[i][0] >> happiness[i][1] >> happiness[i][2];
+    }
+
+    vector<vector<int>> dp(N, vector<int>(3));
+
+    // Base case
+    dp[0][0] = happiness[0][0];
+    dp[0][1] = happiness[0][1];
+    dp[0][2] = happiness[0][2];
+
+    for (int i = 1; i < N; ++i) {
+        dp[i][0] = max(dp[i-1][1], dp[i-1][2]) + happiness[i][0];
+        dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + happiness[i][1];
+        dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + happiness[i][2];
+    }
+
+    int result = max({dp[N-1][0], dp[N-1][1], dp[N-1][2]});
+    cout << result << endl;
+
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
